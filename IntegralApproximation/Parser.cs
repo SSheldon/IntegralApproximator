beb3635ab3675a13492192aa06abb317382321c7
Steven Sheldon <steven@sasheldon.com>
2012-11-13 12:45:03 -0600

    Switched Parser to use YAMP.

--- IntegralApproximation/Parser.cs
+++ IntegralApproximation/Parser.cs
@@ -1,64 +1,45 @@
 ﻿using System;
-using System.Runtime.InteropServices;
-using MTPARSERCOMLib;
+using System.Collections;
+using YAMP;
 
 namespace IntegralApproximation
 {
     public class Parser
     {
-        MTParserClass parser;
-        MTDoubleVectorClass batchVar;
-        MTDoubleClass singleVar;
-
-        public Parser()
-        {
-            parser = new MTParserClass();
-            parser.defineConst("pi", Math.PI);
-            parser.defineConst("π", Math.PI);
-            parser.defineConst("e", Math.E);
-            singleVar = new MTDoubleClass();
-            singleVar.create("x", 0);
-            batchVar = new MTDoubleVectorClass();
-            batchVar.create("x");
-        }
-
         public double Evaluate(string expression)
         {
-            return parser.evaluate(expression);
+            YAMP.Parser parser = YAMP.Parser.Parse(expression);
+            return (parser.Execute() as ScalarValue).Value;
         }
 
         public double[] Evaluate(string function, double[] x)
         {
             double[] y = new double[x.Length];
-            parser.defineVar(batchVar as IMTVariable);
-            parser.compile(function);
-            batchVar.setValueVector(x);
-            parser.evaluateCompiledBatch(x.Length, y);
-            parser.undefineAllVars();
+            Hashtable values = new Hashtable();
+            YAMP.Parser parser = YAMP.Parser.Parse(function);
+            for (int i = 0; i < x.Length; i++)
+            {
+                values["x"] = x[i];
+                y[i] = (parser.Execute(values) as ScalarValue).Value;
+            }
             return y;
         }
 
         public double Evaluate(string function, double x)
         {
-            parser.defineVar(singleVar as IMTVariable);
-            parser.compile(function);
-            singleVar.setValue(x);
-            double y = parser.evaluateCompiled();
-            parser.undefineAllVars();
-            return y;
-
-            //double[] xArray = { x };
-            //return Function(xArray)[0]; 
+            Hashtable values = new Hashtable { { "x", x } };
+            YAMP.Parser parser = YAMP.Parser.Parse(function);
+            return (parser.Execute(values) as ScalarValue).Value;
         }
 
         public bool IsValidExpression(string expression)
         {
             try
             {
-                parser.evaluate(expression);
+                Evaluate(expression);
                 return true;
             }
-            catch (COMException)
+            catch (YAMPException)
             {
                 return false;
             }
@@ -68,14 +49,11 @@ namespace IntegralApproximation
         {
             try
             {
-                parser.defineVar(batchVar as IMTVariable);
-                parser.compile(function);
-                parser.undefineAllVars();
+                Evaluate(function, 0);
                 return true;
             }
-            catch (COMException)
+            catch (YAMPException)
             {
-                parser.undefineAllVars();
                 return false;
             }
         }
